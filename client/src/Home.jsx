ac4cfe512c7e75f3582fd4cb3a2c14cdabf93530
jamyu <jamyu@umich.edu>
2024-11-04 11:14:26 -0500

    for some reason 127.0.0.1 doesnt work but connecting through localhost does

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -2,11 +2,14 @@ import useWebSocket from 'react-use-websocket'
 
 export function Home({username}) {
 
-    const WS_URL = 'ws://127.0.0.1:8000'
-    console.log(WS_URL);
+    //const WS_URL = 'ws://127.0.0.1:8000'
+    const WS_URL = 'ws://localhost:8000'
+
     const { sendJsonMessage } = useWebSocket(WS_URL, {
         queryParams: {username},
     })
+
+    
 
     return <h1>Hello {username}</h1>
 }
