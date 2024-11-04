6660dfeeb7ed1efab234a752bc32a7b0d20df578
jamyu <jamyu@umich.edu>
2024-11-04 14:44:48 -0500

    got everything to work, added cursors log positions and render on the page as cursor changes

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,15 +1,65 @@
+import { useEffect, useRef} from 'react'
 import useWebSocket from 'react-use-websocket'
+import throttle from 'lodash.throttle'
+import { Cursor } from './components/Cursor'
+
+
+const renderCursors = users => {
+    return Object.keys(users).map(uuid => {
+        const user = users[uuid]
+
+        return (
+            <Cursor key = {uuid} point = {[user.state.x, user.state.y]}/>
+        )
+    })
+}
+
+const renderUsersList = users => {
+    return( 
+        <u1>
+            {Object.keys(users).map(uuid => {
+                return <li key = {uuid}>{JSON.stringify(users[uuid])}</li>
+            })}
+        </u1>
+    )
+}
 
 export function Home({username}) {
 
     //const WS_URL = 'ws://127.0.0.1:8000'
     const WS_URL = 'ws://localhost:8000'
 
-    const { sendJsonMessage } = useWebSocket(WS_URL, {
+    const { sendJsonMessage, lastJsonMessage } = useWebSocket(WS_URL, {
         queryParams: {username},
     })
 
-    
+    const THROTTLE = 50 //50ms
+    const sendJsonMessageThrottled = useRef(throttle(sendJsonMessage, THROTTLE))
+
+
+    useEffect(() => {
+        //ask the server to send everyones state the second we load the component   
+        sendJsonMessage({
+            x: 0,
+            y: 0
+        })
+
+        window.addEventListener("mousemove", e => {
+            sendJsonMessageThrottled.current({
+                x: e.clientX,
+                y: e.clientY
+            })
+        })
+    }, [])
+
+    if(lastJsonMessage){
+        return (
+            <>
+                {renderCursors(lastJsonMessage)}
+                {renderUsersList(lastJsonMessage)}
+            </>
+        )
+    }
 
     return <h1>Hello {username}</h1>
 }
